b3a907b0bf4375840a0ec153c4010598d7289a7d
omairapalacios <cpalaciosanchez@hotmail.com>
2020-08-21 14:00:51 -0500

    Auto-generated commit

---
+++ assets/data/data.ts
@@ -0,0 +1,99 @@
+
+export const DATA = [
+  {
+    alias: 'El Profesor',
+    nombre: 'Sergio Marquina',
+    interprete: 'Álvaro Morte',
+    bio: 'El cerebro de la operación. Un hombre que nadie asociaría a priori a ningún plan criminal. Dialogante, de fina ironía y autodidacta, es la antítesis de cualquiera de los individuos que integran su banda. De inteligencia superior, ha planeado durante años hasta el mínimo detalle de la operación. Parece saberlo todo, anticiparse a todo desde su puesto de observación en el exterior. No obstante, no contaba con establecer un vínculo tan fuerte con la negociadora policial, una mujer que le va a marcar.',
+    img: 'assets/img/Profesor.jpeg',
+    grupo: 'atracadores'
+  },
+  {
+    alias: 'Río',
+    nombre: 'Aníbal Cortés',
+    interprete: 'Miguel Herrán',
+    bio: 'Una infancia programando y hackeando. Una adolescencia con la mirada en la Red. Esa capacidad y esos conocimientos le hicieron jefe técnico de una empresa de seguridad siendo aún muy joven. En lo personal, todo ese saber no vino acompañado de un grado de madurez equiparable. Así, pronto se vio metido en hackeos y roturas de sistemas de seguridad que poco tenían de juego. Terminó en búsqueda y captura por la Interpol, situación de la que le rescató El Profesor. Es el maestro tecnológico del grupo.',
+    img: 'assets/img/Rio.png',
+    grupo: 'atracadores'
+  },
+  {
+    alias: 'Berlín',
+    nombre: 'Andrés de Fonollosa Gonzalvez',
+    interprete: 'Pedro Alonso',
+    bio: 'Es el jefe al mando en el interior de la Fábrica de Moneda. Ladrón de guante blanco especializado en el robo de joyas. Ingresó en el mundo del crimen para mantener el altísimo nivel de vida al que estaba acostumbrado desde la cuna. Es un hombre que sabe tener modales finos, que se cree superior al resto de la banda, a quienes desprecia íntimamente. Pero también sabe ser brutal y directo cuando conviene. Su talón de Aquiles son las mujeres, y esta será la circunstancia que ponga en riesgo su liderazgo.',
+    img: 'assets/img/Berlin.jpeg',
+    grupo: 'atracadores'
+  },
+  {
+    alias: 'Denver',
+    nombre: 'Daniel Ramos',
+    interprete: 'Jaime Lorente',
+    bio: 'Hijo de Moscú. Macarra de extrarradio, un poco corto de miras e impulsivo. Alternó su juventud entre los botellones del parking del Caprabo y las barracas de la feria. Coqueteó con todas las drogas posibles y quizás por tal abuso, hay una conexión en su cerebro que salta a “modo irascible” sin motivo alguno. Genera tensión por donde pasa, y es mejor no tratar en exceso con él. Su padre es su único anclaje con la humanidad y los sentimientos. Haría cualquier cosa por él.',
+    img: 'assets/img/Denver.png',
+    grupo: 'atracadores'
+  },
+  {
+    alias: 'Nairobi',
+    nombre: 'Ágata Jiménez',
+    interprete: 'Alba Flores',
+    bio: 'Una joven con una dura infancia que tuvo que ganarse la vida realizando falsificaciones. Embarazada y abandonada por un novio que tuvo en la adolescencia, no vio otra opción que el delito para salir adelante. Su faceta "creativa" le vino muy bien, y pronto era una cotizada falsificadora de todo tipo de cosas, siempre que tuvieran valor material. Tras pasar por prisión, perdió la custodia del pequeño. Con este golpe aspira a recuperarlo y retirarse con él a un lugar tranquilo.',
+    img: 'assets/img/Nairobi.jpeg',
+    grupo: 'atracadores'
+  },
+  {
+    alias: 'Helsinki',
+    nombre: 'Yashin Dasáyev',
+    interprete: 'Darko Peric',
+    bio: 'Serbio. Es la fuerza bruta del grupo. Poco se sabe de él, más allá del evidente pasado de violencia y dureza que ha vivido; y la poca inclinación al diálogo y a hacerse preguntas ante las dificultades que muestra en el presente. Es el soldado de choque perfecto en el plan de El Profesor.',
+    img: 'assets/img/Helsinki.png',
+    grupo: 'atracadores'
+  },
+{
+    alias: 'Lisboa',
+    nombre: 'Raquel Murillo',
+    interprete: 'Itziar Ituño',
+    bio: 'La inspectora, responsable policial del secuestro. A la Inspectora Raquel Murillo este asunto le viene en el peor momento. El secuestro con rehenes que tiene que negociar le cae en la fiesta infantil de su niña Paula, de 7 años. Con una tensión creciente en su vida por la gestión imposible de una custodia arrebatada a su ex marido. No obstante, y casi para su sorpresa, la burbuja en la que está a punto de ingresar, 24 horas al día durante once días, le da mucho más de lo que le quita.	',
+    img: 'assets/img/Lisboa.jpeg',
+    grupo: 'policias'
+  },
+  {
+    alias: 'Estocolmo',
+    nombre: 'Mónica Gaztambide',
+    interprete: 'Esther Acebo',
+    bio: 'Mónica Gaztambide es de esas mujeres que se fija siempre en el hombre equivocado en el peor momento posible. Tras varios fracasos en la oposiciones a funcionario de alto rango, Mónica logró aprobar el examen que le permitía ejercer como secretaria del Director de la Fábrica Nacional de Moneda y Timbre, a la sazón, Arturo, el hombre que le trae por la calle de la amargura. Casado y con tres hijos, lleva tiempo, desde que se liaron prácticame',
+    img: 'assets/img/Estocolmo.png',
+    grupo: 'atracadores'
+  },
+  {
+    alias: 'Tokio',
+    nombre: 'Silene Oliveira',
+    interprete: 'Úrsula Corberó',
+    bio: 'Dura, impulsiva, con una aparente falta de empatía (aunque esto sólo sea una pose). Su vida siempre fue un balancín. Tokio ingresó en el mundo del crimen con 14 años siguiendo los pasos de su novio, de 28. Desde entonces ha alternado periodos de normalidad, con su empleo convencional, con otros ligados a todo tipo de atracos. Eso sí, cada vez más audaces. En uno de ellos, el asalto a una furgoneta de Prosegur, se vio envuelta en un tiroteo con el resultado de tres muertos, uno de ellos su novio. En búsqueda y captura desde entonces.',
+    img: 'assets/img/Tokio.png',
+    grupo: 'atracadores'
+  },
+  {
+    alias: 'Palermo',
+    nombre: 'Martín Barroti',
+    interprete: 'Rodrigo de la Serna',
+    bio: 'Es un ingeniero, brillante en su profesión, pero con muchos conflictos personales y traumas que resolver en su vida; un tipo apasionado que tiene un dolor muy hondo en su corazón y que viene un poco a vengar ese dolor.',
+    img: 'assets/img/Palermo.jpg',
+    grupo: 'atracadores'
+  },
+  {
+    alias: 'Marsella',
+    nombre: 'Desconocido',
+    interprete: 'Luka Peroš',
+    bio: 'Marsella, un hombre parco, de pocas palabras, veterano y víctima de la guerra de los Balcanes, de la que al menos sacó una cosa: la experiencia de hacer el trabajo que nadie quiere hacer para ganarse la vida.',
+    img: 'assets/img/Marsella.jpg',
+    grupo: 'atracadores'
+  },
+  {
+    alias: 'Bógota',
+    nombre: 'Desconocido',
+    interprete: 'Hovik Keuchkerian',
+    bio: 'Bogotá, un tipo que da mucho miedo pero que en el fondo debe tener buen corazón. Ha llegado al grupo de los asaltantes porque tiene un maravilloso don a la hora de hacer agujeros, y aunque sea algo taciturno y lobo solitario, va a dejar alguna escena memorable.',
+    img: 'assets/img/Bogota.png',
+    grupo: 'atracadores'
+  }
+];
